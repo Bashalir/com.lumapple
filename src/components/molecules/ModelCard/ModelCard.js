92ee64accb2dfe9acb1202724a137ee2b2b98cde
bashalir <bachir@sahali.fr>
2020-08-15 02:15:38 +0200

    Add ModelCard

---
+++ src/components/molecules/ModelCard/ModelCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {CardImage} from '../../atoms/card/Card';
+import {defaultTheme} from '../../../styles';
+import StyledModelCard from './style/StyledModelCard';
+import Label from '../../atoms/label/Label';
+
+export default function ModelCard({image, name}) {
+  return (
+    <StyledModelCard>
+      <figure>
+        <CardImage image={image} name={name} />
+      </figure>
+      <Label title={name} />
+    </StyledModelCard>
+  );
+}
+
+ModelCard.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+};
